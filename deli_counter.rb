5512dae8897be9f5c77de03995f85a31341f2351
Nathan Gezahegne <nathandaniel2222@gmail.com>
2018-08-04 16:34:14 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+
+katz_deli = ['jj','kk','tt' ]
+
+def line(katz_deli_line)
+  line_info = []
+  katz_deli_line.each_with_index do |people, index|
+    line_info.push("#{index + 1}. #{people}")
+  end
+  "The line is currently: #{line_info}"
+end
+
+
+    
