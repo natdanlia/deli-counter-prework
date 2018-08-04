adca144546a553994d70bc99c4c745342fe1dc35
Nathan Gezahegne <nathandaniel2222@gmail.com>
2018-08-04 16:51:52 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(katz_deli_line)
   line_info = []
   if katz_deli_line.length > 0 
   katz_deli_line.each_with_index do |people, index|
-    line_info.push("#{index + 1}. #{people}")
+    line_info.push("#{index + 1}. #{people} ")
   end
   "The line is currently: #{line_info.join(",")}"
   else
