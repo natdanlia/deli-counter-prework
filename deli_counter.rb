816858cbc24ac7dcafb1983aab177db04e65d7b3
Nathan Gezahegne <nathandaniel2222@gmail.com>
2018-08-04 17:01:29 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(katz_deli_line)
   katz_deli_line.each_with_index do |people, index|
     line_info.push(" #{index + 1}. #{people}")
   end
-  "The line is currently:#{line_info.join(" ")}"
+  "The line is currently:#{line_info.join("")}"
   else
     "The line is currently empty."
   end
