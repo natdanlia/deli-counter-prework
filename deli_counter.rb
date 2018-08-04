d696b90420f276841a759b98a5be1c6f5e097466
Nathan Gezahegne <nathandaniel2222@gmail.com>
2018-08-04 17:20:08 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,11 @@
 def line(katz_deli_line)
   if katz_deli_line.empty?
-       puts "The line is currently empty."
+    puts "The line is currently empty."
   else
   line_info = []
-  katz_deli_line.each_with_index(1) do |people, index|
-    line_info.push(" #{index}. #{people}")
+  katz_deli_line.each_with_index do |people, index|
+    line_info.push(" #{index + 1}. #{people}")
     end
-   puts  "The line is currently:#{line_info.join("")}"
+   puts "The line is currently:#{line_info.join("")}"
   end
 end
